df78c61c2bd82ceb2494c77bafcd21eb36f970eb
Chad Etzel <chad@jazzychad.net>
2014-08-03 11:40:31 -0700

    convert views to bootstrap 3

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.configure(function(){
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-//  app.locals.pretty = true;
+  //app.locals.pretty = true;
 });
 
 app.configure('production', function(){
